0e291b2a9ea9dc07326062e33663b467ece6bc57
theuxZn <matheusestevam421@gmail.com>
2026-01-10 14:39:19 -0300

    First steps in Java with Spring

---
+++ src/main/java/TheuxZn16/com/github/model/Greeting.java
@@ -0,0 +1,5 @@
+package TheuxZn16.com.github.model;
+
+public record Greeting(long id, String content) {
+
+}
---
+++ src/test/java/TheuxZn16/com/github/StartupTests.java
@@ -0,0 +1,13 @@
+package TheuxZn16.com.github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
